baa2791b27d6898ff753bc1fe5afbd44dc7a7d9f
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-17 12:10:27 +0200

    Added validation for :name and added test cases for it
    Pending to add a name validation with regex to not allow number or any special character

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 } #, format: { with: /\A[\w'\-,.][^0-9_!¡?÷?¿\/\\+=@#$%\ˆ&*()\{\}|~<>;:[\]]{3,}\z/i }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 describe User, type: :model do
-  describe 'validation' do
-    it 'returns false when name is empty' do
+  describe "validation" do
+    it "returns false when name is empty" do
       expect(User.new(name: nil).valid?).to be false
     end
+
+    it "returns false when name has less than 3 chars" do
+      expect(User.new(name: "An").valid?).to be false
+    end
+
+    # it "returns true when name does not contains numbers" do
+    #   expect(User.new(name: "Andrew1").valid?).to be true
+    # end
   end
 end
