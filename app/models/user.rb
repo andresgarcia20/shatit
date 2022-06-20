465e6f67299933bd83a9972562ce8882afc60d2d
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-20 12:53:19 +0200

    Added test cases cases to check uniqueness and fixed :name regex
    Test check uniqueness of :email, :phone_number and :nickname
    Added :name test case for when it contains numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters minimum" } #, format: { with: /\A[A-Za-z]{2,40}[\D]*[^&%$·"()<>?¿¡!@º*;\^\{\}=\-_]*\z/ }
+  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters minimum" }, format: { with: /\A[^0-9]+\z/ }
   validates :surname, presence: true
   validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/ }, uniqueness: true
   validates :phone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,17 @@ describe User, type: :model do
       age: 21
     )}
 
+    let(:repUser) {
+      {
+        name: "Paco",
+        surname: "Li",
+        email: "paco@cw.com",
+        phone_number: 123123123,
+        nickname: "paquito",
+        age: 100
+      }
+    }
+
     it "returns false when name is empty" do
       newUser.name = nil
       expect(newUser.valid?).to be false
@@ -20,9 +31,10 @@ describe User, type: :model do
       expect(newUser.valid?).to be false
     end
 
-    # it "returns true when name does not contains numbers" do
-    #   expect(newUser.new(name: "Andrew20").valid?).to be true
-    # end # Uncomment when regex works
+    it "returns false when name contains numbers" do
+      newUser.name = "Andrew20"
+      expect(newUser.valid?).to be false
+    end
 
     it "returns false when surname is empty" do
       newUser.surname = nil
@@ -48,6 +60,11 @@ describe User, type: :model do
       expect(newUser.valid?).to be false
     end
 
+    context "checks email uniqueness" do
+      before { User.create!({ **repUser, email: "andrew@cw.com" }) }
+      it { expect(newUser).to be_invalid }
+    end
+
     it "returns false when phone number is empty" do
       newUser.phone_number = nil
       expect(newUser.valid?).to be false
@@ -62,6 +79,21 @@ describe User, type: :model do
       expect(newUser.valid?).to be true
     end
 
+    it "returns false if it's only one digit" do
+      newUser.phone_number = 2
+      expect(newUser.valid?).to be false
+    end
+
+    it "returns false eif it's more than 20 digits" do
+      newUser.phone_number = 123123123123123123123
+      expect(newUser.valid?).to be false
+    end
+
+    context "checks phone number uniqueness" do
+      before { User.create!({ **repUser, phone_number: 123131313 }) }
+      it { expect(newUser).to be_invalid }
+    end
+
     it "returns false if when nickname is empty" do
       newUser.nickname = nil
       expect(newUser.valid?).to be false
@@ -69,6 +101,11 @@ describe User, type: :model do
 
     it "returns true if nickname size is >= 2 and <= 10" do
       expect(newUser.valid?).to be true
+    end
+
+    context "checks nickname uniqueness" do
+      before { User.create!({ **repUser, nickname: "drewan" }) }
+      it { expect(newUser).to be_invalid }
     end
 
     it "returns false when age is empty" do
@@ -84,6 +121,11 @@ describe User, type: :model do
     it "returns false if user age is over 100" do
       newUser.age = 101
       expect(newUser.valid?).to be false
+    end
+
+    it "returns true if age is in range" do
+      newUser.age = 50
+      expect(newUser.valid?).to be true
     end
   end
 end
