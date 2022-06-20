062c437a0e8aae5dc135f5df412d9457c100fe7d
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-20 14:43:05 +0200

    Changed camelCase names to snake_case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 describe User, type: :model do
   describe "validation" do
-    let(:newUser) {
+    let(:new_user) {
       User.new(
       name: "Andrew",
       surname: "Cheng",
@@ -10,7 +10,7 @@ describe User, type: :model do
       age: 21
     )}
 
-    let(:repUser) {
+    let(:rep_user) {
       {
         name: "Paco",
         surname: "Li",
@@ -22,115 +22,115 @@ describe User, type: :model do
     }
 
     it "returns false when name is empty" do
-      newUser.name = nil
-      expect(newUser.valid?).to be false
+      new_user.name = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns false when name has less than 3 chars" do
-      newUser.name = "An"
-      expect(newUser.valid?).to be false
+      new_user.name = "An"
+      expect(new_user.valid?).to be false
     end
 
     it "returns false when name contains numbers" do
-      newUser.name = "Andrew20"
-      expect(newUser.valid?).to be false
+      new_user.name = "Andrew20"
+      expect(new_user.valid?).to be false
     end
 
     it "returns false when surname is empty" do
-      newUser.surname = nil
-      expect(newUser.valid?).to be false
+      new_user.surname = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns true when surname is not empty" do
-      expect(newUser.valid?).to be true
+      expect(new_user.valid?).to be true
     end
 
     it "returns false when surname contains numbers" do
-      newUser.name = "Cheng20"
-      expect(newUser.valid?).to be false
+      new_user.name = "Cheng20"
+      expect(new_user.valid?).to be false
     end
 
     it "returns false when email is empty" do
-      newUser.email = nil
-      expect(newUser.valid?).to be false
+      new_user.email = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns true if it's a valid email format" do
-      newUser.email = "andres@andres.com"
-      expect(newUser.valid?).to be true
+      new_user.email = "andres@andres.com"
+      expect(new_user.valid?).to be true
     end
 
     it "returns false if it contains blank spaces" do
-      newUser.email = "andre s@cw.com"
-      expect(newUser.valid?).to be false
+      new_user.email = "andre s@cw.com"
+      expect(new_user.valid?).to be false
     end
 
     context "checks email uniqueness" do
-      before { User.create!({ **repUser, email: "andrew@cw.com" }) }
-      it { expect(newUser).to be_invalid }
+      before { User.create!({ **rep_user, email: "andrew@cw.com" }) }
+      it { expect(new_user).to be_invalid }
     end
 
     it "returns false when phone number is empty" do
-      newUser.phone_number = nil
-      expect(newUser.valid?).to be false
+      new_user.phone_number = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns false if it's not digits" do
-      newUser.phone_number = "asd"
-      expect(newUser.valid?).to be false
+      new_user.phone_number = "asd"
+      expect(new_user.valid?).to be false
     end
 
     it "returns true if it's only digits" do
-      expect(newUser.valid?).to be true
+      expect(new_user.valid?).to be true
     end
 
     it "returns false if it's only one digit" do
-      newUser.phone_number = 2
-      expect(newUser.valid?).to be false
+      new_user.phone_number = 2
+      expect(new_user.valid?).to be false
     end
 
     it "returns false eif it's more than 20 digits" do
-      newUser.phone_number = 123123123123123123123
-      expect(newUser.valid?).to be false
+      new_user.phone_number = 123123123123123123123
+      expect(new_user.valid?).to be false
     end
 
     context "checks phone number uniqueness" do
-      before { User.create!({ **repUser, phone_number: 123131313 }) }
-      it { expect(newUser).to be_invalid }
+      before { User.create!({ **rep_user, phone_number: 123131313 }) }
+      it { expect(new_user).to be_invalid }
     end
 
     it "returns false if when nickname is empty" do
-      newUser.nickname = nil
-      expect(newUser.valid?).to be false
+      new_user.nickname = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns true if nickname size is >= 2 and <= 10" do
-      expect(newUser.valid?).to be true
+      expect(new_user.valid?).to be true
     end
 
     context "checks nickname uniqueness" do
-      before { User.create!({ **repUser, nickname: "drewan" }) }
-      it { expect(newUser).to be_invalid }
+      before { User.create!({ **rep_user, nickname: "drewan" }) }
+      it { expect(new_user).to be_invalid }
     end
 
     it "returns false when age is empty" do
-      newUser.age = nil
-      expect(newUser.valid?).to be false
+      new_user.age = nil
+      expect(new_user.valid?).to be false
     end
 
     it "returns false if user is underage" do
-      newUser.age = 17
-      expect(newUser.valid?).to be false
+      new_user.age = 17
+      expect(new_user.valid?).to be false
     end
 
     it "returns false if user age is over 100" do
-      newUser.age = 101
-      expect(newUser.valid?).to be false
+      new_user.age = 101
+      expect(new_user.valid?).to be false
     end
 
     it "returns true if age is in range" do
-      newUser.age = 50
-      expect(newUser.valid?).to be true
+      new_user.age = 50
+      expect(new_user.valid?).to be true
     end
   end
 end
