f8fac593eb90bcd64502b83e5062fd9bd9701639
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-20 09:19:16 +0200

    Added :username validation in User model and added test cases for it
    Added validation for not being blank and added test cases such it cant be empty and when username and name together are not empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3 } #, format: { with: /\A[A-Za-z]{2,40}[\D]*[^&%$·"()<>?¿¡!@º*;\^\{\}=\-_]*\z/ }
+  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters minimum" } #, format: { with: /\A[A-Za-z]{2,40}[\D]*[^&%$·"()<>?¿¡!@º*;\^\{\}=\-_]*\z/ }
   validates :surname, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,19 @@ describe User, type: :model do
     end
 
     # it "returns true when name does not contains numbers" do
-    #   expect(User.new(name: "Andrew").valid?).to be true
-    # end
+    #   expect(User.new(name: "Andrew20").valid?).to be true
+    # end # Uncomment when regex works
 
-    it "returns false when name is empty" do
+    it "returns false when surname is empty" do
       expect(User.new(surname: nil).valid?).to be false
     end
+
+    it "returns true when name and surname are not empty" do
+      expect(User.new(name: "Andrew", surname: "La").valid?).to be true
+    end
+
+    # xit "returns false when email is empty" do
+    #   expect(User.new(email: nil).valid?).to be false
+    # end
   end
 end
