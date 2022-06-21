4945e43966806626c528573153fa50316cf9796f
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-21 14:35:56 +0200

    Created new migration to add enum field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_111748) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_122544) do
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.string "surname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_111748) do
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
