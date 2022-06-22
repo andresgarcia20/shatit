7454d4b66cac59322e58a4a90a4b8938bcdcdd25
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-22 14:41:16 +0200

    Created test to check if Vehicle is valid and for :model validation

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 describe Vehicle, type: :model do
   describe "validation" do
-    
+    before {
+      @new_vehicle = create(:vehicle)
+    }
+
+    it "returns true when vehicle is valid" do
+      expect(@new_vehicle).to be_valid
+    end
+
+    it "returns false if model is empty" do
+      @new_vehicle.model = nil
+      expect(@new_vehicle).to be_invalid
+    end
+
+    it "returns false if model length is < 3" do
+      @new_vehicle.model = "BM"
+      expect(@new_vehicle).to be_invalid
+    end
   end
 end
