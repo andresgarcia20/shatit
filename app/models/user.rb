a5f9809fb085238a67687553e90a7083f8cf9917
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-22 14:24:42 +0200

    Fixed enum syntax in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :phone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :nickname, presence: true, length: { minimum: 2,  maximum: 10 }, uniqueness: true
   validates :birthday, presence: true
-  enum role: [:admin: 20, :driver: 10, :passenger: 0, :banned: 30], default: :passenger
+  enum role: { admin: 20, driver: 10, passenger: 0, banned: 30 }, _default: 0
 
   def adult?
     age >= 18
