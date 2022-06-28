0285e525d70b8fa54dfa9ce0cf58929a037b33d1
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-28 14:23:02 +0200

    Rename method and created new method to know size of destinations array

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,15 +7,19 @@ class Trip < ApplicationRecord
   validates :destinations, presence: true, length: { minimum: 1, maximum: 9 }
   validates :available_seats, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :departure_date, presence: true
+  validate :date_valid?
 
-  def expired?
-    check_if_date_valid == true
+  def date_valid?
+    current_date = Time.zone.now
+    valid = current_date < departure_date
+    unless valid
+      errors.add(:departure_date, "departure date must be after #{current_date.to_date}")
+    end
+    valid
   end
 
-  private
-
-  def check_if_date_valid
-    current_date = Time.zone.now
-    current_date > departure_date
+  def number_of_stops
+    # Number of stops without counting the final destination
+    destination.size - 1
   end
 end
