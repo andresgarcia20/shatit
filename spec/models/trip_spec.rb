1f63ddcd394370284acf7f2cb470f2660cc08376
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-30 16:46:29 +0200

    Added scopes test

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,7 +1,6 @@
 describe User, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
   describe "validation" do
-    include ActiveSupport::Testing::TimeHelpers
-
     before do
       travel_to Time.zone.local(2022, 06, 28)
       @new_trip = create(:trip)
@@ -69,5 +68,36 @@ describe User, type: :model do
         it { expect(@new_trip.date_valid?).to be true }
       end
     end
+  end
+
+  describe "scope" do
+    before do
+      travel_to Time.zone.local(2022, 06, 28)
+      @trip = create(:trip)
+    end
+
+    it "by_origin returns info by given origin" do
+      expect(Trip.by_origin("Madrid")).to eq([@trip])
+    end
+
+    it "by_destination returns info by given destination" do
+      expect(Trip.by_destination("Valencia")).to eq([@trip])
+    end
+
+    it "by_number_of_stops returns info by given stops" do
+      expect(Trip.by_number_of_stops(1).count).to eq(@trip.number_of_stops)
+    end
+
+    # it "by_vehicle returns info by given type of vehicle" do
+    #   expect(Trip.by_vehicle(4)).to include(@trip.vehicle)
+    # end
+
+    it "by_free_seats returns info by given number of free seats" do
+      expect(Trip.by_free_seats(1)).to eq([@trip])
+    end
+
+    # it "by_user returns info by given user" do
+    #   expect(Trip.by_user()).to eq([@trip])
+    # end
   end
 end
