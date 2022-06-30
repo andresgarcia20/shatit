adbed80011687620a85dab63dbbc3add3bd0304d
Andres Garcia <andres.garcia@coverwallet.com>
2022-06-30 10:56:54 +0200

    Added vehicle association to its factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -27,5 +27,6 @@ FactoryBot.define do
     destinations { ["Cuenca", "Valencia"] }
     available_seats { 1 }
     departure_date { "2022-06-29 12:00:00" }
+    vehicle { association :vehicle }
   end
 end
