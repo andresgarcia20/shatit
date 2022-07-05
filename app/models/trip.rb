140cfe25bf40c5d9aa25d42f86c836cb53366145
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-05 17:05:12 +0200

    Added new filter to save info in db in lowercase
    Useful for better control in filters

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,6 +7,7 @@ class Trip < ApplicationRecord
   validates :available_seats, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :departure_date, presence: true
   validate :date_valid?
+  before_save :downcase_fields
 
   def date_valid?
     current_date = Time.zone.now
@@ -22,10 +23,15 @@ class Trip < ApplicationRecord
     destinations.size - 1
   end
 
+  def downcase_fields
+    origin.downcase!
+    destinations.map { |el| el.downcase! }
+  end
+
   scope :by_origin, ->(origin) { where("origin = ?", origin) }
   scope :by_destination, ->(destination) { where("? = ANY (destinations)", destination) }
-  scope :by_number_of_stops, ->(num) { where("ARRAY_LENGTH(destinations, 1) = ?", (num + 1)) }
+  scope :by_number_of_stops, ->(stops) { where("ARRAY_LENGTH(destinations, 1) = ?", (stops + 1)) }
   scope :by_vehicle, ->(type) { joins(:vehicle).where("Vehicles.vehicle_type = ?", type) }
-  scope :by_free_seats, ->(num) { where("available_seats = ?", num) }
+  scope :by_free_seats, ->(seats) { where("available_seats = ?", seats) }
   scope :by_user, ->(user_id) { where("user_id = ?", user_id) }
 end
