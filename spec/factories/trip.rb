27d9d49d6dca96f6daf0add0f34eb294d0a73391
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-10 14:50:56 +0200

    Added faker in trip factory

--- spec/factories/trip.rb
+++ spec/factories/trip.rb
@@ -1,10 +1,12 @@
+require "faker"
+
 FactoryBot.define do
   factory :trip do
     user { association :user }
     origin { "Madrid" }
     destinations { ["Cuenca", "Valencia"] }
     available_seats { 1 }
-    departure_date { "2022-06-29 12:00:00" }
+    departure_date { Faker::Date.between(from: 1.day.from_now, to: 6.months.from_now) }
     vehicle { association :vehicle }
     pets { 1 }
     luggage { 2 }
