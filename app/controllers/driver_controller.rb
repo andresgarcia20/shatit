3637f205bc20f141cedf43831ff6e84b3d30209a
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-12 10:30:04 +0200

    Implemented scope change

--- app/controllers/driver_controller.rb
+++ app/controllers/driver_controller.rb
@@ -4,9 +4,8 @@ class DriverController < ApplicationController
   end
 
   def index
-    query = Trip.by_user(current_user.id)
-    query = query.trips_done if params[:filter] == "done"
-    query = query.trips_todo if params[:filter] == "todo"
+    query = Trip.by_user(current_user.id).order("departure_date")
+    query = query.trips_date_range(params[:start_date], params[:end_date]) if params[:start_date].present? || params[:end_date].present?
     @trips = query.all
   end
 end
