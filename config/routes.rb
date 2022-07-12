6d7c2c99594aad8c55444eb1d632a52682053ce7
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-12 10:43:25 +0200

    Implement suggestion with routes organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get "driver", to: "driver#index"
   devise_for :users, controllers: { sessions: "users/sessions" }
   root "home#index"
   resources :trips
 
   resources :users do
+    member do
+      get "driver", to: "driver#index"
+    end
     resources :vehicles
   end
 end
