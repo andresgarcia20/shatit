9e33a91b6509beafd65e01b668b26cdafb92fee6
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-15 10:03:14 +0200

    Used faker library to seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 # # ------------ Users -----------
-# # ADMIN
-# User.create(name: "Andrés", surname: "García", nickname: "drewan", phone_number: 123123123, email: "andres@cw.com", birthday: "2001/03/20", role: "admin")
 
-# # DRIVER
-# User.create(name: "Paco", surname: "Perez", nickname: "paquirrin", phone_number: 123123124, email: "paco@cw.com", birthday: "1990/01/02", role: "driver")
-
-# # PASSENGER
-# User.create(name: "Sara", surname: "Puertas", nickname: "sarinha", phone_number: 123123125, email: "sara@cw.com", birthday: "2001/03/20")
-
-# # BANNED
-# User.create(name: "Fer", surname: "Cheng", nickname: "fer123", phone_number: 123123126, email: "fer@cw.com", birthday: "2001/03/20", role: "banned")
+# 50.times do
+#   user = User.new(
+#     name: Faker::Name.first_name,
+#     surname: Faker::Name.middle_name,
+#     nickname: Faker::Internet.username(specifier: 3..10),
+#     phone_number: Faker::Number.number(digits: 9),
+#     email: Faker::Internet.email,
+#     birthday: Faker::Date.birthday(min_age: 18, max_age: 100),
+#     password: Faker::Internet.password(min_length: 6),
+#   )
+#   user.save!
+# end
 
 # # ------------- Vehicles ---------------
 
@@ -23,74 +25,74 @@
 
 # #--------------- Trips ------------------
 
-Trip.create(
-  user_id: 1,
-  origin: "MADRID",
-  destinations: ["BARCELONA", "CORDOBA"],
-  available_seats: 1,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 1,
-  pets: 0,
-  luggage: 0,
-  gasoline_comission: 100,
-)
+# Trip.create(
+#   user_id: 1,
+#   origin: "MADRID",
+#   destinations: ["BARCELONA", "CORDOBA"],
+#   available_seats: 1,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 1,
+#   pets: 0,
+#   luggage: 0,
+#   gasoline_comission: 100,
+# )
 
-Trip.create(
-  user_id: 1,
-  origin: "VALENCIA",
-  destinations: ["GRANADA", "SEVILLA"],
-  available_seats: 1,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 1,
-  pets: 0,
-  luggage: 0,
-  gasoline_comission: 85,
-)
+# Trip.create(
+#   user_id: 1,
+#   origin: "VALENCIA",
+#   destinations: ["GRANADA", "SEVILLA"],
+#   available_seats: 1,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 1,
+#   pets: 0,
+#   luggage: 0,
+#   gasoline_comission: 85,
+# )
 
-Trip.create(
-  user_id: 2,
-  origin: "MALAGA",
-  destinations: ["CADIZ", "BILBAO"],
-  available_seats: 3,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 2,
-  pets: 1,
-  luggage: 3,
-  gasoline_comission: 150,
-)
+# Trip.create(
+#   user_id: 2,
+#   origin: "MALAGA",
+#   destinations: ["CADIZ", "BILBAO"],
+#   available_seats: 3,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 2,
+#   pets: 1,
+#   luggage: 3,
+#   gasoline_comission: 150,
+# )
 
-Trip.create(
-  user_id: 2,
-  origin: "MADRID",
-  destinations: ["SALAMANCA", "BILBAO", "BARCELONA"],
-  available_seats: 2,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 3,
-  pets: 1,
-  luggage: 1,
-  gasoline_comission: 150,
-)
+# Trip.create(
+#   user_id: 2,
+#   origin: "MADRID",
+#   destinations: ["SALAMANCA", "BILBAO", "BARCELONA"],
+#   available_seats: 2,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 3,
+#   pets: 1,
+#   luggage: 1,
+#   gasoline_comission: 150,
+# )
 
-Trip.create(
-  user_id: 5,
-  origin: "CORDOBA",
-  destinations: ["VALENCIA", "GRANADA"],
-  available_seats: 6,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 4,
-  pets: 2,
-  luggage: 4,
-  gasoline_comission: 90,
-)
+# Trip.create(
+#   user_id: 5,
+#   origin: "CORDOBA",
+#   destinations: ["VALENCIA", "GRANADA"],
+#   available_seats: 6,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 4,
+#   pets: 2,
+#   luggage: 4,
+#   gasoline_comission: 90,
+# )
 
-Trip.create(
-  user_id: 5,
-  origin: "SEVILLE",
-  destinations: ["MALAGA", "CADIZ", "MADRID", "SALAMANCA"],
-  available_seats: 4,
-  departure_date: "2022-07-20 12:00:00",
-  vehicle_id: 4,
-  pets: 0,
-  luggage: 3,
-  gasoline_comission: 120,
-)
+# Trip.create(
+#   user_id: 5,
+#   origin: "SEVILLE",
+#   destinations: ["MALAGA", "CADIZ", "MADRID", "SALAMANCA"],
+#   available_seats: 4,
+#   departure_date: "2022-07-20 12:00:00",
+#   vehicle_id: 4,
+#   pets: 0,
+#   luggage: 3,
+#   gasoline_comission: 120,
+# )
