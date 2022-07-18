cd301145c542e5c36fdec360d834ba39767bf1ef
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-18 11:24:19 +0200

    Changed kids column type to string array

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_084036) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_091924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_18_084036) do
     t.integer "companions"
     t.integer "pets"
     t.integer "luggage"
-    t.string "kids"
+    t.string "kids", default: [], array: true
     t.integer "stage"
     t.bigint "user_id", null: false
     t.bigint "trip_id", null: false
