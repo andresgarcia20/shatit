2d2241f542d941d4f16bdc5630795710a6394c48
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-19 17:30:01 +0200

    Completed TripJoinRequest tests

--- spec/routing/trip_join_requests_routing_spec.rb
+++ spec/routing/trip_join_requests_routing_spec.rb
@@ -3,36 +3,35 @@ require "rails_helper"
 RSpec.describe TripJoinRequestsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/trip_join_requests").to route_to("trip_join_requests#index")
+      expect(get: "/trips/1/trip_join_requests").to route_to("trip_join_requests#index", trip_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/trip_join_requests/new").to route_to("trip_join_requests#new")
+      expect(get: "/trips/1/trip_join_requests/new").to route_to("trip_join_requests#new", trip_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/trip_join_requests/1").to route_to("trip_join_requests#show", id: "1")
+      expect(get: "/trips/1/trip_join_requests/1").to route_to("trip_join_requests#show", id: "1", trip_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/trip_join_requests/1/edit").to route_to("trip_join_requests#edit", id: "1")
+      expect(get: "/trips/1/trip_join_requests/1/edit").to route_to("trip_join_requests#edit", id: "1", trip_id: "1")
     end
 
-
     it "routes to #create" do
-      expect(post: "/trip_join_requests").to route_to("trip_join_requests#create")
+      expect(post: "/trips/1/trip_join_requests").to route_to("trip_join_requests#create", trip_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/trip_join_requests/1").to route_to("trip_join_requests#update", id: "1")
+      expect(put: "/trips/1/trip_join_requests/1").to route_to("trip_join_requests#update", id: "1", trip_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/trip_join_requests/1").to route_to("trip_join_requests#update", id: "1")
+      expect(patch: "/trips/1/trip_join_requests/1").to route_to("trip_join_requests#update", id: "1", trip_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/trip_join_requests/1").to route_to("trip_join_requests#destroy", id: "1")
+      expect(delete: "/trips/1/trip_join_requests/1").to route_to("trip_join_requests#destroy", id: "1", trip_id: "1")
     end
   end
 end
