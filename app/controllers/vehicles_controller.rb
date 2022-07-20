839f2bd10932b65640455b68cbd47577280a31a0
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-20 17:20:14 +0200

    Removed useless content

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,27 +1,22 @@
 class VehiclesController < ApplicationController
   before_action :set_vehicle, only: %i[ show edit update destroy ]
 
-  # GET /vehicles or /vehicles.json
   def index
     @vehicles = Vehicle.all
   end
 
-  # GET /vehicles/1 or /vehicles/1.json
   def show
     @vehicle = Vehicle.find(params[:id])
   end
 
-  # GET /vehicles/new
   def new
     @vehicle = Vehicle.new
   end
 
-  # GET /vehicles/1/edit
   def edit
     @vehicle = Vehicle.find(params[:id])
   end
 
-  # POST /vehicles or /vehicles.json
   def create
     @user = User.find(params[:user_id])
     @vehicle = @user.vehicles.create(vehicle_params)
@@ -37,7 +32,6 @@ class VehiclesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /vehicles/1 or /vehicles/1.json
   def update
     respond_to do |format|
       if @vehicle.update(vehicle_params)
@@ -50,7 +44,6 @@ class VehiclesController < ApplicationController
     end
   end
 
-  # DELETE /vehicles/1 or /vehicles/1.json
   def destroy
     @user = User.find(params[:user_id])
     @vehicle.destroy
@@ -63,12 +56,10 @@ class VehiclesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_vehicle
     @vehicle = Vehicle.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def vehicle_params
     params.require(:vehicle).permit(:user_id, :model, :air_conditioning, :vehicle_type, :consumption, :nickname)
   end
