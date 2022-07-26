5f03160b9d51771b7229e93f31b82e0afd7eb303
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-26 13:11:45 +0200

    Added put routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,15 @@ Rails.application.routes.draw do
     resources :trip_join_requests do
       member do
         patch "accepted", to: "accepted#update"
+        put "accepted", to: "accepted#update"
         patch "payment_in_progress", to: "payment_in_progress#update"
+        put "payment_in_progress", to: "payment_in_progress#update"
         patch "paid", to: "paid#update"
+        put "paid", to: "paid#update"
         patch "canceled", to: "canceled#update"
+        put "canceled", to: "canceled#update"
         patch "rejected", to: "rejected#update"
+        put "rejected", to: "rejected#update"
       end
     end
   end
