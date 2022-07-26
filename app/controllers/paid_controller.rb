9137e2dd2ca0598d4cc783a7823872a96c3b11d3
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-26 13:13:24 +0200

    Created controllers for each stage

---
+++ app/controllers/paid_controller.rb
@@ -0,0 +1,25 @@
+class PaidController < ApplicationController
+  before_action :set_paid_trip_join_request, only: %i[ update ]
+
+  def update
+    respond_to do |format|
+      if @trip_join_request.update(trip_join_request_params)
+        format.html { redirect_to trip_trip_join_request_url(id: params[:id]), notice: "Trip join request stage was successfully updated." }
+        format.json { render trip_trip_join_request_url(id: params[:id]), status: :created, location: @trip_join_request.trip_id }
+      else
+        format.html { render trip_trip_join_request_url(id: params[:id]), status: :unprocessable_entity }
+        format.json { render json: @trip_join_request.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_paid_trip_join_request
+    @trip_join_request = TripJoinRequest.find(params[:id])
+  end
+
+  def trip_join_request_params
+    params.require(:trip_join_request).permit(:companions, :pets, :luggage, :stage, :trip_id, kids: [])
+  end
+end
