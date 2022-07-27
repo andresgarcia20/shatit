8fba672fab776db81ed23132827458cfcb397bf6
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-27 16:20:56 +0200

    Added validation to reject all request with same date

--- app/services/TripJoinRequestStageManager.rb
+++ app/services/TripJoinRequestStageManager.rb
@@ -6,6 +6,10 @@ class TripJoinRequestStageManager
       seats = trip.available_seats - (req.companions + REQUESTER)
       trip.update_column(:available_seats, seats)
 
+      user_requests = req.user.trip_join_requests.select { |elm| elm.stage == "requested" && elm.id != req.id }
+      requests_trip_date_equal = user_requests.select { |elm| elm.trip.departure_date.to_date == trip.departure_date.to_date }
+
+      requests_trip_date_equal.each { |elm| elm.update_column(:stage, "rejected") }
       req.accepted!
     else
       raise "Invalid stage"
