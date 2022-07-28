d4baf0655855b4932d044e8de47b0299213ec6d3
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-28 13:24:18 +0200

    Created and implemented two scopes to do filtering in database

--- app/services/TripJoinRequestStageManager.rb
+++ app/services/TripJoinRequestStageManager.rb
@@ -2,43 +2,46 @@ require "./app/services/InvalidStageChange"
 
 class TripJoinRequestStageManager
   REQUESTER = 1
-  def self.accept!(req)
-    if req.requested?
-      trip = req.trip
-      seats = trip.available_seats - (req.companions + REQUESTER)
+  def self.accept!(trip_request)
+    if trip_request.requested?
+      trip = trip_request.trip
+      seats = trip.available_seats - (trip_request.companions + REQUESTER)
       trip.update_column(:available_seats, seats)
 
-      user_requests = req.user.trip_join_requests.select { |elm| elm.stage == "requested" && elm.id != req.id }
-      requests_trip_date_equal = user_requests.select { |elm| elm.trip.departure_date.to_date == trip.departure_date.to_date }
-      requests_trip_date_equal.each { |elm| elm.update_column(:stage, "rejected") }
+      user_overlapping_requests = trip_request.user.trip_join_requests.by_stage_requested.where("trip_join_requests.id != #{trip_request.id}").by_trip_date(trip.departure_date)
+      user_overlapping_requests.map(&:rejected!)
 
-      req.accepted!
+      trip_request.accepted!
     else
       raise InvalidStageChange
     end
   end
 
-  def self.payment_in_progress!(req)
-    if req.accepted?
-      req.payment_in_progress!
+  def self.payment_in_progress!(trip_request)
+    if trip_request.accepted?
+      trip_request.payment_in_progress!
     else
       raise InvalidStageChange
     end
   end
 
-  def self.paid!(req)
-    if req.payment_in_progress?
-      req.paid!
+  def self.paid!(trip_request)
+    if trip_request.payment_in_progress?
+      trip_request.paid!
     else
       raise InvalidStageChange
     end
   end
 
-  def self.reject!(req)
-    if req.requested?
-      req.rejected!
+  def self.reject!(trip_request)
+    if trip_request.requested?
+      trip_request.rejected!
     else
       raise InvalidStageChange
     end
+  end
+
+  def self.canceled!(trip_request)
+    trip_request.canceled!
   end
 end
