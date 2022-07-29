5ffc6fe4b8fd5c4bb24778497a6aa66cb2fc764d
Andres Garcia <andres.garcia@coverwallet.com>
2022-07-29 16:21:12 +0200

    Applied SRP to update columns in Stage Manager

--- app/services/TripJoinRequestStageManager.rb
+++ app/services/TripJoinRequestStageManager.rb
@@ -7,9 +7,9 @@ class TripJoinRequestStageManager
 
     trip = trip_request.trip
     new_seats = trip.available_seats - (trip_request.companions + REQUESTER)
-    trip.update_column(:available_seats, new_seats)
     new_pets = trip.pets - trip_request.pets
-    trip.update_column(:pets, new_pets)
+
+    trip.update_availability({ seats: (trip_request.companions + REQUESTER), pets: trip_request.pets })
 
     user_overlapping_requests = trip_request.user.trip_join_requests.by_stage_requested.where("trip_join_requests.id != #{trip_request.id}").by_trip_date(trip.departure_date)
     user_overlapping_requests.map(&:rejected!)
