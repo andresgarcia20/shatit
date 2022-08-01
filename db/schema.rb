5a39cd51bfc90132b5a9806ef76cac6fac8b9b5f
Andres Garcia <andres.garcia@coverwallet.com>
2022-08-01 11:28:42 +0200

    Created migration to rename companions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_083633) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_092657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "companions_tables", force: :cascade do |t|
+  create_table "companions", force: :cascade do |t|
     t.string "name"
     t.string "surname"
     t.integer "phone_number"
