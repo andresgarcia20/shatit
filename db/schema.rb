ef2160f64c4411c9e9ce1698cf380dccef63c2a5
Andres Garcia <andres.garcia@coverwallet.com>
2022-08-01 10:31:11 +0200

    Created migration to add requesters_list columns as an array of json Added in TripJoinRequest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_105732) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_082815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_105732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.jsonb "requesters_list", default: [], array: true
     t.index ["trip_id"], name: "index_trip_join_requests_on_trip_id"
     t.index ["user_id"], name: "index_trip_join_requests_on_user_id"
   end
