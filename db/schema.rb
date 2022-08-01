5d5e0ccc0a210f6171c178482bc5e7180f584add
Andres Garcia <andres.garcia@coverwallet.com>
2022-08-01 10:38:28 +0200

    Created Companions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_082815) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_083633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companions_tables", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trip_join_requests", force: :cascade do |t|
     t.integer "companions"
