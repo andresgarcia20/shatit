94cd7c2f8098460ebd90ea755796767595c9c74f
Andres Garcia <andres.garcia@coverwallet.com>
2022-08-01 11:44:02 +0200

    Created validations for Companion model

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,2 +1,5 @@
 class Companion < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters minimum" }, format: { with: /\A[^0-9]+\z/ }
+  validates :surname, presence: true, format: { with: /\A[^0-9]+\z/ }
+  validates :phone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 2, maximum: 20 }, uniqueness: true
 end
