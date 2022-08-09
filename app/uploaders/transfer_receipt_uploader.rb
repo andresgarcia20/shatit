aa830bbebfae40b511201c75d43fce313023d247
Andres Garcia <andres.garcia@coverwallet.com>
2022-08-09 12:24:07 +0200

    Generated transfer_receipt uploader config

--- app/uploaders/transfer_receipt_uploader.rb
+++ app/uploaders/transfer_receipt_uploader.rb
@@ -13,6 +13,14 @@ class TransferReceiptUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_allowlist
+    %w(jpg jpeg png)
+  end
+
+  def content_type_allowlist
+    /image\//
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
