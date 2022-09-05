635e921f3fca2b49de1d16f08695e41194e99b8f
Andrés García <playsharp59@gmail.com>
2022-09-05 16:25:00 +0200

    Updated driver request factory to include images

--- spec/factories/driver_requests.rb
+++ spec/factories/driver_requests.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :driver_request do
-    user { nil }
-    driver_licence_front { "MyString" }
-    driver_licence_back { "MyString" }
+    user_id { create(:user).id }
+    driver_licence_front { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'test_images', 'sad_cat_croc.jpg')) }
+    driver_licence_back { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'test_images', 'sad_cat_croc.jpg')) }
+    rejection_reason { "" }
+
+    trait :rejection_reason do
+      rejection_reason { "Invalid images." }
+    end
   end
 end
