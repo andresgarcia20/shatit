562ab0272cd21c3ba8ae3189003cec4414a34e06
Andrés García <playsharp59@gmail.com>
2022-09-07 13:12:40 +0200

    Configured uploaders and implement changes to views

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,15 @@
 class Vehicle < ApplicationRecord
   belongs_to :user, required: true
   has_one :trips
+  mount_uploader :insurance, VehicleUploader
+  mount_uploader :car_revision, VehicleUploader
 
   validates :model, length: { minimum: 3 }
   validates :air_conditioning, inclusion: { in: [true, false] }
   validates :consumption, presence: true, numericality: { only_float: true, greater_than: 0 }
   validates :nickname, presence: true, length: { minimum: 2, maximum: 10 }
+  validates :insurance, presence: true
+  validates :car_revision, presence: true
 
   enum vehicle_type: { car: 0, van: 1, boat: 2, yatch: 3, motorbike: 4, bike: 5, caravan: 6 }
 end
