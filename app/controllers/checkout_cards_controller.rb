333b45365294d2bfbf3290ef986cc234b64af57f
andresgarcia20 <andres.garcia@coverwallet.com>
2022-09-14 10:45:41 +0200

    Finished basic config for payment

--- app/controllers/checkout_cards_controller.rb
+++ app/controllers/checkout_cards_controller.rb
@@ -1,21 +1,24 @@
 class CheckoutCardsController < ApplicationController
   def create
-    product = TripJoinRequest.find(params[:id])
+    product = TripJoinRequest.find(params[:trip_join_request_id])
     trip = Trip.find(params[:trip_id])
 
     @session = Stripe::Checkout::Session.create({
-      success_url: "https://example.com/success",
-      cancel_url: "https://example.com/cancel",
-      line_items: [
-        { name: trip.id,
-          amount: trip.trip_price,
-          trip_request: product.id,
-          currency: "euro",
-          quantity: 1 },
-      ],
+      success_url: trip_trip_join_request_url(trip_id: trip.id, id: product.id),
+      cancel_url: root_url,
+      line_items: [{
+        price_data: { unit_amount: trip.trip_price,
+                     currency: "eur",
+                     product_data: { name: "#{trip.origin} - #{trip.destinations.last}",
+                                     metadata: { trip: trip.id } } },
+        quantity: 1,
+      }],
+      metadata: { trip_request: product.id },
       mode: "payment",
     })
 
-    binding.pry
+    respond_to do |format|
+      format.js
+    end
   end
 end
